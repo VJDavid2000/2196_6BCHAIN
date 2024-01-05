7dc423809e0386ab6cc266457b7e81ad3b89e045
VJDavid2000 <98504050+VJDavid2000@users.noreply.github.com>
2024-01-05 15:28:22 +0800

    Add files via upload

---
+++ React_Practice/First_React/Hello.jsx
@@ -0,0 +1,11 @@
+export default function Hello() {
+    let message = "I Have Done It Here!";
+    return(
+        <>
+        <div>
+            <p>Hello World!</p>
+            <h2>{message}</h2>
+        </div>
+        </>
+    );
+}
---
+++ React_Practice/First_React/Welcome.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import React from 'react';
+
+class Welcome extends React.Component {
+    render() {
+        return(
+            <div>
+                <h3>Welcome, Vincent Jericho David!</h3>
+            </div>
+        );
+    }
+}
+
+export default Welcome;
